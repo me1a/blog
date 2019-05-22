6113c9ce52be7bf3fcc0b203077c388720fe896b
me1a <mxh4373@gmail.com>
2019-05-22 14:08:32 +0800

    添加less处理

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
-const fs = require('fs')
-
 const { parallel, series, src, dest, watch } = require('gulp')
 
 const browserSync = require('browser-sync').create()
 
 const myplugin = require('./build/gulp-md2html')
 
+const less = require('gulp-less')
+const mincss = require('gulp-clean-css')
+
 
 
 function md2html() {
   return src('article/**/*.md').pipe(myplugin()).pipe(dest('dist/'))
 }
+function less2css() {
+  return src('less/*.less')
+    .pipe(less({}))
+    .pipe(mincss())
+    .pipe(dest('dist/static/css/'))
+}
 
 function watchTask() {
+  watch('less/**/*.less', parallel(less2css))
   watch('article/**/*.md', parallel(md2html))
 }
 
@@ -30,4 +38,4 @@ function server(cb) {
 
 
 
-exports.default = series(md2html, server, watchTask)
+exports.default = series(parallel(md2html, less2css), server, watchTask)
