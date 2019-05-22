50cc8c065d8113ca5349c0fda79db3b9aecfd110
me1a <mxh4373@gmail.com>
2019-05-22 16:04:47 +0800

    添加 非md 页面

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,33 @@
 const { parallel, series, src, dest, watch } = require('gulp')
-
-const browserSync = require('browser-sync').create()
-const rm = require('rimraf')
-
-const md2obj = require('./build/gulp-md2obj')
-const pug2html = require('./build/gulp-pug2html')
-
 const less = require('gulp-less')
 const mincss = require('gulp-clean-css')
+const browserSync = require('browser-sync').create()
+const rm = require('rimraf')
+const pug = require('gulp-pug')
+
+const md2obj = require('./build/gulp-md2obj')
+const obj2pug = require('./build/gulp-pug2html')
+
+
 
 
 
 function doc2html() {
-  return src('article/**/*.md').pipe(md2obj()).pipe(pug2html(
+  return src('article/**/*.md').pipe(md2obj()).pipe(obj2pug(
     { template: process.cwd() + '/article/template.pug' }
-  )).pipe(dest('dist/'))
+  )).pipe(dest('dist/article'))
 }
 function less2css() {
   return src('less/*.less')
     .pipe(less({}))
     .pipe(mincss())
     .pipe(dest('dist/static/css/'))
+}
+function pug2html() {
+  return src('pages/**/*.pug')
+    .pipe(pug({
+      locals: {}
+    })).pipe(dest('dist'))
 }
 
 
@@ -51,5 +58,5 @@ function server(cb) {
 
 
 
-exports.default = series(clean, parallel(doc2html, less2css), server, watchTask)
-exports.build = series(clean, parallel(doc2html, less2css))
+exports.default = series(clean, parallel(doc2html, less2css, pug2html), server, watchTask)
+exports.build = series(clean, parallel(doc2html, less2css, pug2html))
