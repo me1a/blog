6c81f0e46062db0ca1fac62096edfe96d35e44f7
me1a <mxh4373@gmail.com>
2019-06-05 15:38:13 +0800

    .

--- build/md/getTitle.js
+++ build/md/getTitle.js
@@ -4,7 +4,6 @@ module.exports = function getFrontMatter() {
     const children = tree.children
     children.forEach(item => {
       if (item.type === 'element' && item.tagName === 'h1') {
-        console.log(item.properties.id)
         data._h1 = item.properties.id
       }
     })
